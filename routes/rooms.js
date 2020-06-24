dafaef85b8fa779e12c65ddaa4b68d97f90e250c
Mansur <baymns10@gmail.com>
2020-06-24 19:39:43 +0300

    Get rooms list from db and add form

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,31 +1,35 @@
 const router = require('express').Router()
-const Room = require('../models/room')
+const Room = require('../models/room.js')
 
 router.get('/', async (req, res) => {
   const rooms = await Room.find();
+
   res.render('rooms/roomslist', { rooms });
 })
-router.route('/create')
-  .get((req, res) => {
-    res.render('roomform');
-  })
-  .post(async (req, res) => {
-    try {
-      const { title, shortUrl } = req.body;
-      const room = new Room({
-        title, shortUrl
-      });
-      await room.save();
-      res.redirect(`/rooms/${room._id}`);
-    } catch (error) {
-      res.redirect('/rooms');
-    }
-  })
+
+router.get('/create', (req, res) => {
+  res.render('rooms/roomform');
+})
+router.post('/create', async (req, res) => {
+  try {
+    const {title} = req.body
+    const room = new Room({
+      title
+    });
+    await room.save();
+    res.redirect(`/rooms/${room._id}`);
+  } catch (error) {
+    console.log(error)
+    res.redirect('/rooms');
+  }
+})
 router.get('/:id', async (req, res) => {
   try {
     const { id } = req.params;
+    console.log(id);
+
     const room = await Room.findOne({ id });
-    res.render('rooms/room', {room})
+    res.render('rooms/room', { room })
   } catch (error) {
     res.redirect('/rooms')
   }
