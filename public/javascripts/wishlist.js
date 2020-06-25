0cf5fade7f1f852fe71a1bf2c991e49e9856da84
Alexandr Kuznetsov <kuznecov.alexandr@gmail.com>
2020-06-25 17:10:29 +0300

    Some changes

--- public/javascripts/wishlist.js
+++ public/javascripts/wishlist.js
@@ -2,18 +2,18 @@ const wishlist = document.querySelector('.wishlist');
 wishlist?.addEventListener('click', (event) => {
   if (event.target.classList.contains('add-product')) {
     const addForm = document.querySelector('.add-one-product');
-    addForm.classList.toggle('invisible');
+    addForm?.classList.toggle('invisible');
   }
   if (event.target.classList.contains('checkbox')) {
-    console.log('boom');
-    event.target.parerntElement.classList.toggle('bg-success');
+    event.target.parentElement.children[1].classList.toggle('bg-success');
   }
 });
 
 const addForm = document.querySelector('.add-one-product');
 addForm?.addEventListener('click', (event) => {
   if (event.target.classList.contains('add-row-button')) {
-    console.log('Клик на кнопку добавить');
-    // TODO: fetch на загрузку позиции в БД и перезагрузка формы
+    // TODO: запись в БД и перезагрузка страницы
+
+    window.location.reload();
   }
 });
